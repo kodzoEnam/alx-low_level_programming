144e77b90da0860e4e3d60b63df348ff40f61f25
Caleb Agbittor <cagbittor@gmail.com>
2023-02-16 21:11:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,15 +4,15 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
-in main(void)
+int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 
 }
